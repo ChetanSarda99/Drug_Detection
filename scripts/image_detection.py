e25fe71b202a7035b1cb1c0bf6ee6eb462bb2d64
ChetanSarda99 <sarda.chetan1999@gmail.com>
2023-12-30 14:21:57 -0800

    notebook added

--- scripts/image_detection.py
+++ scripts/image_detection.py
@@ -1,18 +1,23 @@
-import easyocr
 import cv2
-image_path = r'C:\Users\sarda\OneDrive\Desktop\PythonCodes\PotfolioProjects\ImageRecogWebScrape\data\raw\images\Acetaminophen--L484--1.jpg'
-def easy_ocr_image_to_text(image_path):
-    # Create a reader object
-    reader = easyocr.Reader(['en'])  # pass language code
-    # Read the image
-    image = cv2.imread(image_path)
-    # Extract text from image
-    result = reader.readtext(image)
-    # Process result
-    text = ' '.join([res[1] for res in result])
-    return text
+import pytesseract
+from PIL import Image
 
-# Example usage
-image_path = 'path_to_your_image.jpg'
-text = easy_ocr_image_to_text(image_path)
-print(text)
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+image_path = r'C:\Users\sarda\OneDrive\Desktop\PythonCodes\PotfolioProjects\ImageRecogWebScrape\data\raw\images\Atorvastatin Calcium--APO ATV20--1.jpg'
+image = cv2.imread(image_path)
+
+# Check if the image is read correctly
+if image is None:
+    print("The image could not be read. Please check the file path.")
+else:
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, binary_image = cv2.threshold(gray_image, 90, 215, cv2.THRESH_BINARY)
+    inverted_image = cv2.bitwise_not(binary_image)
+    text = pytesseract.image_to_string(inverted_image)
+    print("Detected text:", text)
+    #show image static
+    cv2.imshow("Image", inverted_image)
+    cv2.waitKey(0)
